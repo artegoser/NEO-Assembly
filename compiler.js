f0d65021711ad36f5d6781429d7e65d01334568d
Artemy <artegoser@gmail.com>
2022-01-29 13:54:27 +0300

    variables

--- compiler.js
+++ compiler.js
@@ -17,37 +17,77 @@ let commandmap = {
   shl: 14,
 };
 
+let variables = {};
+
+function convertNums(strnum) {
+  if (strnum[strnum.length - 1] === "h") return parseInt(strnum, 16);
+  else if (strnum[strnum.length - 1] === "b") return parseInt(strnum, 2);
+  else if (strnum[strnum.length - 1] === "&") return variables[strnum];
+  else return parseInt(strnum);
+}
+
+function filtNone(arr) {
+  return arr.filter((v) => v);
+}
+
 function compile(path, opath) {
   let file = fs.readFileSync(path, "utf-8");
   file += "\n";
   file = file.replace(/\/\/(.*?)\r?\n/gi, "\n");
   let asmcommands = file.split(/\r?\n/);
+  asmcommands = filtNone(asmcommands);
   let stream = fs.createWriteStream(opath);
   let commands = [];
   let data = [];
   let addrs = [];
-  for (let asmcommand of asmcommands) {
-    if (!asmcommand) continue;
-    let [command, datac] = asmcommand.split(" ");
-    if (datac[datac.length - 1] === "h") data.push(parseInt(datac, 16));
-    else if (datac[datac.length - 1] === "b") data.push(parseInt(datac, 2));
-    else data.push(parseInt(datac));
-    commands.push(commandmap[command]);
+  let memoryaddr = 0;
+  let totalsets = 0;
+  for (let i = 0; i < asmcommands.length; i++) {
+    let [command, ...args] = asmcommands[i].split(" ");
+    if (command === "set") totalsets += 1;
   }
-  for (let i = 0; i < commands.length; i++) {
-    addrs.push(commands.length + i);
+  for (let i = 0; i < asmcommands.length; i++) {
+    let [command, ...args] = asmcommands[i].split(" ");
+    args = filtNone(args);
+
+    if (command === "load") {
+      commands.push(1);
+      addrs.push(convertNums(args[0]));
+      continue;
+    }
+
+    if (command !== "set") {
+      commands.push(commandmap[command]);
+      data.push(convertNums(args[0]));
+
+      if (commands[i] !== 0 || command !== "load")
+        addrs.push(asmcommands.length - totalsets + i - 1);
+    } else {
+      variables[args[0]] = asmcommands.length + memoryaddr - totalsets;
+      data.push(convertNums(args[1]));
+    }
+    memoryaddr += 1;
   }
+
   for (let i = 0; i < commands.length; i++) {
     stream.write(Buffer.alloc(1, commands[i]));
     stream.write(Buffer.alloc(1, addrs[i]));
   }
-  for (let i = 0; i < commands.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     let datastring = data[i].toString(16);
     stream.write(
       Buffer.alloc(2, "0".repeat(4 - datastring.length) + datastring, "hex")
     );
   }
+  console.log(commands);
+  console.log(data);
+  console.log(addrs);
+  console.log(variables);
   console.timeEnd("Compiled");
 }
 
 module.exports = compile;
+
+// [ 1, 3, 5, 4, 6 ]
+// [ 2, 3, 2, 2, 4 ]
+// [ 5, 6, 7, 8, 9 ]
